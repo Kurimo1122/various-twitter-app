5036e5ac4104302185134dbb60afa7d2b66236fd
Kurimo1122 <kugimoto1122@gmail.com>
2016-10-24 23:12:53 +0900

    test

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def index():
 
 @app.route('/word_cloud/<user_id>', methods=['GET', 'POST'])
 def word_cloud(user_id):
-    
-    timeline = request.form['action']
+    if request.method == 'POST':    
+        timeline = request.form['action']
     print(timeline[0].text)
     text_list = []
     wakati_list = []
