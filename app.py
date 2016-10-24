9fad36a07b2fe02dfc2841b86df7c44164db6456
Kurimo1122 <kugimoto1122@gmail.com>
2016-10-25 05:27:30 +0900

    test

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ def twitter_auth():
         # Save the request_token which will be used after authentication
         session['request_token'] = auth.request_token
     
-    except (tweepy.TweepError, e):
-        logging.error(str(e))
+    except (tweepy.TweepError):
+        #logging.error(str(e))
     
     return redirect(redirect_url)
 
