70c07064904d08f75b21860156f10b62d76ab2dc
Kurimo1122 <kugimoto1122@gmail.com>
2016-11-03 17:56:33 +0900

    test

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def index():
     wakati_text = tagger.parse(text_all)
     
     # calculation of the sentiment score
-    
+    """
     for word in wakati_text:
         if '名詞'.decode('utf-8') in word.feature:
             wakati_list.append(word.surface)
@@ -114,8 +114,8 @@ def index():
             adjs.append(word.surface)
         if '副詞'.decode('utf-8') in word.feature:
             advs.append(word.surface)
-    
     """
+    
     for word in wakati_text:
         if '名詞' in word.feature:
             wakati_list.append(word.surface)
@@ -126,7 +126,7 @@ def index():
             adjs.append(word.surface)
         if '副詞' in word.feature:
             advs.append(word.surface)
-    """
+    
 
     score = number = 0
     score_n, number_n = analyze(nouns,nounswords,nounspoint)
@@ -162,8 +162,7 @@ def word_cloud(user_id):
     
     global wakati_all
     wakati = wakati_all
-    print('wakati_allをprintするよ')
-    print(wakati)
+    
     stop_words = [
         u'こと', u'そう', u'はず', u'みたい', u'それ',
         u'よう', u'こと', u'これ', u'ため', u'せい', 
